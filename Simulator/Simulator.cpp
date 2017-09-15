eb2e4e78d8c6dfadfe0d69a03c4b5e979a6dfbc3
VikingPingvin <sch1458@gmail.com>
2017-09-15 10:59:48 +0200

    GPSTK source directory rearrange. Added test Observation Rinex file in Test project.

--- Simulator/Simulator.cpp
+++ Simulator/Simulator.cpp
@@ -8,32 +8,7 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	double llh[3] = { 0.75, 0.75, 100 };
-	double ecef[3] = {0,0,0};
-	GPSTime gpstime;
-
-	gpstk::Position pos;
-	FULLFrame frame;
-	cout << "Position is: " << pos << endl;
-	pos.setECEF(-1575232.0141, -4707872.2332, 3993198.4383);
-	cout << "Position is: " << pos << endl;
-	cout << "ECEF transformed to LLH(Geodetic): " << pos.transformTo(gpstk::Position::Geodetic) << endl;
-
-
-	std::cout << "LeapSeconds of 1993.09.21. is: " << gpstk::TimeSystem::getLeapSeconds(1993, 9, 21) << std::endl;
-	cout << "Pre-Debug tests ended" << endl << "---------------------------------" << endl;
-
-	char* path = "..\\SimulatorTest\\TestFiles\\TrajectoryTestFiles\\goodTrajectoryFilewECEF.txt";
-	trajectoryReader trajectoryReader(path);
-	frame = trajectoryReader.readLine_as_frame();
-	cout << frame.GPSTime.WN << " " << frame.GPSTime.ToW << " " << frame.ECEFcoordinates.x << " " << frame.ECEFcoordinates.y << " " << frame.ECEFcoordinates.z << " " << frame.LLHcoordinates.latitude << " " << frame.LLHcoordinates.longitude << " " << frame.LLHcoordinates.height << endl;
-	frame = trajectoryReader.readLine_as_frame();
-	cout << frame.GPSTime.WN << " " << frame.GPSTime.ToW << " " << frame.ECEFcoordinates.x << " " << frame.ECEFcoordinates.y << " " << frame.ECEFcoordinates.z << " " << frame.LLHcoordinates.latitude << " " << frame.LLHcoordinates.longitude << " " << frame.LLHcoordinates.height << endl;
-	frame = trajectoryReader.readLine_as_frame();
-	cout << frame.GPSTime.WN << " " << frame.GPSTime.ToW << " " << frame.ECEFcoordinates.x << " " << frame.ECEFcoordinates.y << " " << frame.ECEFcoordinates.z << " " << frame.LLHcoordinates.latitude << " " << frame.LLHcoordinates.longitude << " " << frame.LLHcoordinates.height << endl;
-	frame = trajectoryReader.readLine_as_frame();
-	cout << frame.GPSTime.WN << " " << frame.GPSTime.ToW << " " << frame.ECEFcoordinates.x << " " << frame.ECEFcoordinates.y << " " << frame.ECEFcoordinates.z << " " << frame.LLHcoordinates.latitude << " " << frame.LLHcoordinates.longitude << " " << frame.LLHcoordinates.height << endl;
-
+	cout << "RINEX data file reading test" << endl;
 	getchar();
 
 	return 0;
